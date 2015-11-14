d4c8dcb7d219b3749f99b047077fd8571adea432
Chris <chris.reeve4@gmail.com>
2015-11-13 22:14:50 -0700

    Month View Improvements

--- src/com/android/calendar/month/MonthListView.java
+++ src/com/android/calendar/month/MonthListView.java
@@ -39,10 +39,10 @@ public class MonthListView extends ListView {
     // fling. Above MULTIPLE_MONTH_VELOCITY_THRESHOLD, do multiple month flings according to the
     // fling strength. When doing multiple month fling, the velocity is reduced by this threshold
     // to prevent moving from one month fling to 4 months and above flings.
-    private static int MIN_VELOCITY_FOR_FLING = 1500;
+    private static int MIN_VELOCITY_FOR_FLING = 500;
     private static int MULTIPLE_MONTH_VELOCITY_THRESHOLD = 8000;
-    private static int FLING_VELOCITY_DIVIDER = 500;
-    private static int FLING_TIME = 1000;
+    private static int FLING_VELOCITY_DIVIDER = 2000;
+    private static int FLING_TIME = 500;
 
     // disposable variable used for time calculations
     protected Time mTempTime;
